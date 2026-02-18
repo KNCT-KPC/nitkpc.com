e53d22b269561f4f81bea8cd34f58d050e4880f3
Arumino <git@arumino-om.net>
2026-02-18 18:22:20 +0900

    feat: 新しいサイトを作成

---
+++ docs/intro.md
@@ -0,0 +1,9 @@
+---
+title: 講習会資料置き場
+sidebar_position: 1
+---
+
+# 準備中・・・
+以下リンクより，旧サイトの資料を取得できます．
+
+https://old.nitkpc.com
---
+++ docusaurus.config.ts
@@ -0,0 +1,150 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: '釧路高専プログラミング研究部',
+  // tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
+
+  plugins: ['docusaurus-plugin-sass'],
+
+  // Set the production url of your site here
+  url: 'https://your-docusaurus-site.example.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ja',
+    locales: ['ja'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          blogSidebarTitle: "全ての投稿",
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'NIT-KPC',
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'documentsSidebar',
+          position: 'left',
+          label: '講習会資料',
+        },
+        {
+          to: '/blog',
+          label: 'ブログ',
+          position: 'left'
+        },
+        {
+          href: 'https://github.com/KNCT-KPC/nitkpc.com',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'SITEMAP',
+          items: [
+            {
+              label: '講習会資料',
+              to: '/docs/intro',
+            },
+            {
+              label: 'ブログ',
+              to: '/blog'
+            }
+          ],
+        },
+        {
+          title: 'PORTAL',
+          items: [
+            {
+              label: '釧路工業高等専門学校',
+              href: 'https://www.kushiro-ct.ac.jp',
+            },
+            {
+              label: '旧プロ研ホームページ',
+              href: 'https://old.nitkpc.com',
+            },
+            {
+              label: 'U-16プログラミングコンテスト 釧路大会',
+              href: 'https://procon.946oss.net',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/KNCT-KPC',
+            },
+          ],
+        },
+      ],
+      copyright: `© ${new Date().getFullYear()} National Institute of Technology, Kushiro College. Programming Club.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
